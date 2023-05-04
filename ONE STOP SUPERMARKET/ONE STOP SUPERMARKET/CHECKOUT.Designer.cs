6627d7d99d570d2042570e278b8023eb20daff45
hellotsoo <132605603+hellotsoo@users.noreply.github.com>
2023-05-05 00:51:34 +0700

    Add files via upload

---
+++ ONE STOP SUPERMARKET/ONE STOP SUPERMARKET/CHECKOUT.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ONE_STOP_SUPERMARKET
+{
+    partial class CHECKOUT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(354, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CHECK OUT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(145, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ITEMS:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "- BARANG 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(145, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "- BARANG 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(308, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Rp.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(308, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Rp.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(145, 205);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Total pembayaran:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Lakukan Pembayaran";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(145, 330);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Alamat:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(302, 326);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(311, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Confirm";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(308, 205);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(24, 13);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Rp.";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(308, 280);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(191, 13);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "*Pembayaran hanaya bisa melalui COD";
+            // 
+            // CHECKOUT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CHECKOUT";
+            this.Text = "CHECKOUT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
---
+++ ONE STOP SUPERMARKET/ONE STOP SUPERMARKET/Form2.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ONE_STOP_SUPERMARKET
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fOODToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sNACKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fROZENFOODToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dRINKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mILKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sODAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aLCOHOLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fRUITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cLEANERSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pERSONALCAREToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pAPERGOODSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mEATFISHToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sNACKSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fOODToolStripMenuItem,
+            this.dRINKToolStripMenuItem,
+            this.fRUITToolStripMenuItem,
+            this.mEATFISHToolStripMenuItem,
+            this.sNACKSToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fOODToolStripMenuItem
+            // 
+            this.fOODToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sNACKToolStripMenuItem,
+            this.fROZENFOODToolStripMenuItem});
+            this.fOODToolStripMenuItem.Name = "fOODToolStripMenuItem";
+            this.fOODToolStripMenuItem.Size = new System.Drawing.Size(108, 29);
+            this.fOODToolStripMenuItem.Text = "PRODUCE";
+            // 
+            // sNACKToolStripMenuItem
+            // 
+            this.sNACKToolStripMenuItem.Name = "sNACKToolStripMenuItem";
+            this.sNACKToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.sNACKToolStripMenuItem.Text = "FRUIT";
+            // 
+            // fROZENFOODToolStripMenuItem
+            // 
+            this.fROZENFOODToolStripMenuItem.Name = "fROZENFOODToolStripMenuItem";
+            this.fROZENFOODToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.fROZENFOODToolStripMenuItem.Text = "VEGETABLES";
+            // 
+            // dRINKToolStripMenuItem
+            // 
+            this.dRINKToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mILKToolStripMenuItem,
+            this.sODAToolStripMenuItem,
+            this.aLCOHOLToolStripMenuItem});
+            this.dRINKToolStripMenuItem.Name = "dRINKToolStripMenuItem";
+            this.dRINKToolStripMenuItem.Size = new System.Drawing.Size(121, 29);
+            this.dRINKToolStripMenuItem.Text = "BEVERAGES";
+            // 
+            // mILKToolStripMenuItem
+            // 
+            this.mILKToolStripMenuItem.Name = "mILKToolStripMenuItem";
+            this.mILKToolStripMenuItem.Size = new System.Drawing.Size(193, 34);
+            this.mILKToolStripMenuItem.Text = "DAIRY";
+            // 
+            // sODAToolStripMenuItem
+            // 
+            this.sODAToolStripMenuItem.Name = "sODAToolStripMenuItem";
+            this.sODAToolStripMenuItem.Size = new System.Drawing.Size(193, 34);
+            this.sODAToolStripMenuItem.Text = "SODA";
+            // 
+            // aLCOHOLToolStripMenuItem
+            // 
+            this.aLCOHOLToolStripMenuItem.Name = "aLCOHOLToolStripMenuItem";
+            this.aLCOHOLToolStripMenuItem.Size = new System.Drawing.Size(193, 34);
+            this.aLCOHOLToolStripMenuItem.Text = "ALCOHOL";
+            // 
+            // fRUITToolStripMenuItem
+            // 
+            this.fRUITToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cLEANERSToolStripMenuItem,
+            this.pERSONALCAREToolStripMenuItem,
+            this.pAPERGOODSToolStripMenuItem});
+            this.fRUITToolStripMenuItem.Name = "fRUITToolStripMenuItem";
+            this.fRUITToolStripMenuItem.Size = new System.Drawing.Size(139, 29);
+            this.fRUITToolStripMenuItem.Text = "HOUSE HOLD";
+            // 
+            // cLEANERSToolStripMenuItem
+            // 
+            this.cLEANERSToolStripMenuItem.Name = "cLEANERSToolStripMenuItem";
+            this.cLEANERSToolStripMenuItem.Size = new System.Drawing.Size(249, 34);
+            this.cLEANERSToolStripMenuItem.Text = "CLEANERS";
+            // 
+            // pERSONALCAREToolStripMenuItem
+            // 
+            this.pERSONALCAREToolStripMenuItem.Name = "pERSONALCAREToolStripMenuItem";
+            this.pERSONALCAREToolStripMenuItem.Size = new System.Drawing.Size(249, 34);
+            this.pERSONALCAREToolStripMenuItem.Text = "PERSONAL CARE";
+            // 
+            // pAPERGOODSToolStripMenuItem
+            // 
+            this.pAPERGOODSToolStripMenuItem.Name = "pAPERGOODSToolStripMenuItem";
+            this.pAPERGOODSToolStripMenuItem.Size = new System.Drawing.Size(249, 34);
+            this.pAPERGOODSToolStripMenuItem.Text = "PAPER GOODS";
+            // 
+            // mEATFISHToolStripMenuItem
+            // 
+            this.mEATFISHToolStripMenuItem.Name = "mEATFISHToolStripMenuItem";
+            this.mEATFISHToolStripMenuItem.Size = new System.Drawing.Size(158, 29);
+            this.mEATFISHToolStripMenuItem.Text = "MEAT AND FISH";
+            // 
+            // sNACKSToolStripMenuItem
+            // 
+            this.sNACKSToolStripMenuItem.Name = "sNACKSToolStripMenuItem";
+            this.sNACKSToolStripMenuItem.Size = new System.Drawing.Size(94, 29);
+            this.sNACKSToolStripMenuItem.Text = "SNACKS";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fOODToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sNACKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fROZENFOODToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dRINKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mILKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sODAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aLCOHOLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fRUITToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cLEANERSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pERSONALCAREToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pAPERGOODSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mEATFISHToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sNACKSToolStripMenuItem;
+    }
+}
